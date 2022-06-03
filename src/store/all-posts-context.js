7b115aed0294345e23d82e297ecbf3e2415ec0aa
PLAWSO <64998692+PLAWSO@users.noreply.github.com>
2022-06-02 20:52:23 -0500

    removed console logs >:(

--- src/store/all-posts-context.js
+++ src/store/all-posts-context.js
@@ -41,19 +41,18 @@ export function AllPostsContextProvider(props) {
     delete post.updatedAt
     delete post.createdAt
     post.likes++
-    console.log(post.likes)
+
     const updatedPost = await API.graphql({query: mutations.updatePost, variables: {input: getPostById(postId)}}).catch((error) => console.log(error))
     let newPosts = posts
-    console.log(updatedPost.data.updatePost)
+
     newPosts = newPosts.map(post => {
       if (post.id === postId){
-        console.log('found')
+
         return updatedPost.data.updatePost
       }
       else
         return post
     })
-    console.log('')
     setAllPosts(() => {
       return newPosts.sort((a, b) => {
         return ('' + b.createdAt).localeCompare(a.createdAt)
